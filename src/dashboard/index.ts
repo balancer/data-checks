73fd4dd4af8a11388f7ddc606be00fe32beabe77
gmbronco <83549293+gmbronco@users.noreply.github.com>
2024-05-21 18:12:09 +0200

    dashboard

--- src/dashboard/index.ts
+++ src/dashboard/index.ts
@@ -1,25 +1,14 @@
 export default {
   // Use this to create an externally callable function
   async fetch(event: FetchEvent, env: Env, ctx: ExecutionContext): Promise<Response> {
-    // Get the ENV variables
-    const DUNE_API_KEY = await env.DATA_CHECKS_ENV.get('DUNE_API_KEY');
+    // Get the check results from KV
+    const aprs = await env.DATA_CHECKS_ENV.get('aprs');
+    const prices = await env.DATA_CHECKS_ENV.get('aprs');
 
-    if (!DUNE_API_KEY) {
-      return new Response('DUNE_API_KEY is not set', { status: 500 });
-    }
-
-    return new Response('OK');
-  },
-  // Use this for scheduled checks
-  async scheduled(event: ScheduledEvent, env: Env, ctx: ExecutionContext): Promise<void> {
-    // Get the ENV variables
-    const DUNE_API_KEY = await env.DATA_CHECKS_ENV.get('DUNE_API_KEY');
-
-    if (!DUNE_API_KEY) {
-      console.error('DUNE_API_KEY is not set');
-      return;
-    }
-
-    console.log('Scheduled event');
+    // Format the response as a simple HTML in pre tags
+    return new Response(
+      `<pre>APR Check Results: ${JSON.stringify(aprs, null, 2)}</pre><pre>Price Check Results: ${JSON.stringify(prices, null, 2)}</pre>`,
+      { headers: { 'Content-Type': 'text/html' } },
+    );
   },
 };
