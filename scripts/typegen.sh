76d7f0f3d6decb641109e5e1b498406dd39be0e0
gmbronco <83549293+gmbronco@users.noreply.github.com>
2024-05-21 14:45:11 +0200

    dashboard

--- scripts/typegen.sh
+++ scripts/typegen.sh
@@ -1,11 +1,6 @@
 run_cf_typegen() {
     folder="$1"
     if [ -d "$folder" ]; then
-        if [ -f "$folder/.dev.vars" ]; then
-            awk 'NR==FNR{a[$1];next}!($1 in a)' .dev.vars "$folder/.dev.vars" >> "$folder/.dev.vars"
-        else
-            cp .dev.vars "$folder"
-        fi
         (cd "$folder" && npm run cf-typegen)
     else
         echo "Directory $folder does not exist."
